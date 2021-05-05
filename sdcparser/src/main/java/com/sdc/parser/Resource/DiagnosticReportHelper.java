43d6ae3593ce8a1de7168560ccacf8b549e0ad71
Christa Wan <christa@christa.local>
2021-05-05 13:17:26 -0400

    Support for DiagnosticReport

--- sdcparser/src/main/java/com/sdc/parser/Resource/DiagnosticReportHelper.java
+++ sdcparser/src/main/java/com/sdc/parser/Resource/DiagnosticReportHelper.java
@@ -44,7 +44,6 @@ public class DiagnosticReportHelper {
         	myList.add(obRef);
         }
         diagReport.setResult(myList);
-        //myList.add(new Reference("Adrenal.Bx.Res.129_3.002.011.RC1_sdcFDF3d1c4fe4-09c3-4a7e-877f-9ddb160da6db/ver1#2118.100004300"));
         
         //presented form in Base64 
         Attachment attachment = new Attachment();
