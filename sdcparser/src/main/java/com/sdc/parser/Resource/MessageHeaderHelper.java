b5d3962795e5db5afcb381672bcfbc4947cbc4ea
Josh Liben <josh@junipercds.com>
2022-04-14 12:49:24 -0400

    added meta profile to header

--- sdcparser/src/main/java/com/sdc/parser/Resource/MessageHeaderHelper.java
+++ sdcparser/src/main/java/com/sdc/parser/Resource/MessageHeaderHelper.java
@@ -1,7 +1,12 @@
 package com.sdc.parser.Resource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.hl7.fhir.r4.model.CanonicalType;
 import org.hl7.fhir.r4.model.MessageHeader;
 import org.hl7.fhir.r4.model.MessageHeader.MessageSourceComponent;
+import org.hl7.fhir.r4.model.Meta;
 import org.hl7.fhir.r4.model.Narrative.NarrativeStatus;
 
 import ca.uhn.fhir.context.FhirContext;
@@ -9,23 +14,18 @@ import ca.uhn.fhir.context.FhirContext;
 public class MessageHeaderHelper {
 
 	private static final String EVENT_CODING_SYSTEM_NAME = "http://example.org/fhir/message-events";
+	private static String profileCanonical = "http://hl7.org/fhir/us/cancer-reporting/StructureDefinition/us-pathology-message-header";
 
 	public static MessageHeader createMessageHeader(FhirContext ctx) {
 		MessageHeader messageHeader = new MessageHeader();
 		messageHeader.getText().setStatus(NarrativeStatus.GENERATED);
-		//messageHeader.getText().setDivAsString("<div>" + MESSAGE_HEADER_TEXT + "</div>");
-		/*
-		 * Narrative messageHeaderNarrative = new Narrative();
-		 * messageHeaderNarrative.setStatus(NarrativeStatus.GENERATED); XhtmlNode
-		 * messageHeaderText = new XhtmlNode(NodeType.Element, "p");
-		 * messageHeaderText.addText(MESSAGE_HEADER_TEXT);
-		 * messageHeaderNarrative.setDiv(messageHeaderText);
-		 * messageHeader.setText(messageHeaderNarrative);
-		 */
+
 		messageHeader.getEventCoding().setSystem(EVENT_CODING_SYSTEM_NAME).setCode("admin-notify");
 		messageHeader.setSource(new MessageSourceComponent().setName("IHE SDC on FHIR Parser")
 				.setEndpoint("http://localhost:8080/sdcparser"));
-//		String encoded = ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(messageHeader);
+		messageHeader.setMeta(new Meta()
+				.setProfile(new ArrayList<CanonicalType>(Arrays.asList(
+						new CanonicalType(profileCanonical)))));
 		return messageHeader;
 	}
 }
