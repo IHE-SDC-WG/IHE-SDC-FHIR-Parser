fa52f42caf82b3b84e986a5704e07a1e0413558f
Josh Liben <josh@junipercds.com>
2022-04-14 14:28:32 -0400

    simplified DiagnosticReportHelper

--- sdcparser/src/main/java/com/sdc/parser/Resource/DiagnosticReportHelper.java
+++ sdcparser/src/main/java/com/sdc/parser/Resource/DiagnosticReportHelper.java
@@ -1,10 +1,9 @@
 package com.sdc.parser.Resource;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import com.sdc.parser.Config.ConfigValues;
-import com.sdc.parser.Config.PatientConfig;
+
 import org.hl7.fhir.r4.model.Attachment;
 import org.hl7.fhir.r4.model.Base64BinaryType;
 import org.hl7.fhir.r4.model.DiagnosticReport;
@@ -15,45 +14,35 @@ import org.hl7.fhir.r4.model.Reference;
 import ca.uhn.fhir.context.FhirContext;
 
 public class DiagnosticReportHelper {
+    private static final String CODING_DISPLAY = "Pathology Synoptic report";
+    private static final String CODING_CODE = "60568-3";
+    private static final String CATEGORY_DISPLAY = "Pathology";
+    private static final String SYSTEM_URL = "http://loinc.org";
+    private static final String CATEGORY_CODE = "LP7839-6";
     private static final String PROFILE_URL = "http://hl7.org/fhir/us/cancer-reporting/StructureDefinition/us-pathology-diagnostic-report";
 
     public static DiagnosticReport createDiagnosticReport(FhirContext ctx, String sdcForm,
             String patientUUID, ArrayList<Observation> observations, ConfigValues configValues) {
 
         DiagnosticReport diagReport = new DiagnosticReport();
-      
-        // meta 
+
         diagReport.getMeta().addProfile(PROFILE_URL);
-        //category 
-        diagReport.getCategoryFirstRep().addCoding().setCode("LP7839-6").setSystem("http://loinc.org").setDisplay("Pathology"); 
-        //code
-        diagReport.getCode().getCodingFirstRep().setCode("60568-3").setSystem("http://loinc.org").setDisplay("Pathology Synoptic report"); 
-        //subject
-        //diagReport.setSubject(new Reference(patientUUID));
-        PatientConfig patientConfig = configValues.getPatientConfig();
-        diagReport.getSubject().setDisplay(patientConfig.getFullName()).setReference("Patient/"
-            + patientConfig.getSystem().values().toArray()[0]); 
-        
-        //status
-
+        diagReport.getCategoryFirstRep().addCoding().setCode(CATEGORY_CODE).setSystem(SYSTEM_URL)
+                .setDisplay(CATEGORY_DISPLAY);
+        diagReport.getCode().getCodingFirstRep().setCode(CODING_CODE).setSystem(SYSTEM_URL).setDisplay(CODING_DISPLAY);
+        diagReport.getSubject().setDisplay(
+                configValues.getPatientConfig().getFullName()).setReference(
+                        "Patient/"
+                                + configValues.getPatientConfig().getSystem().values().toArray()[0]);
         diagReport.setStatus(DiagnosticReportStatus.FINAL);
-        // effective date time
-        // diagReport.setEffective(new DateTimeType());
         diagReport.getEffectiveDateTimeType().setValueAsString("2021-01-01T21:39:30.000Z");
+        diagReport.getResult().addAll(
+                observations.stream().map(ob -> {
+                    Reference reference = new Reference(ob.getIdentifierFirstRep().getValue().replaceAll("/", "."));
+                    reference.getIdentifier().setSystem("https://example.org/").setValue(reference.getReference());
+                    return reference;
+                }).toList());
 
-        // result
-        // diagReport.getResultFirstRep().setReference("Adrenal.Bx.Res.129_3.002.011.RC1_sdcFDF3d1c4fe4-09c3-4a7e-877f-9ddb160da6db/ver1#2118.100004300");
-        List<Reference> myList = diagReport.getResult();
-        for (Observation ob : observations) {
-            Reference obRef = new Reference(ob.getIdentifierFirstRep().getValue().replaceAll("/", "."));
-            myList.add(obRef);
-        }
-        diagReport.setResult(myList);
-        for (Reference r : diagReport.getResult()) {
-            r.getIdentifier().setSystem("https://example.org/").setValue(r.getReference());
-        }
-
-        // presented form in Base64
         Attachment attachment = new Attachment();
         attachment.setContentType("application/xml");
         attachment.setDataElement(new Base64BinaryType(sdcForm.getBytes()));
